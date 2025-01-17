c39300f5e9256eec863ea0cb195882a4299c4ccf
shohira123 <shohira95@gmail.com>
2025-01-17 15:31:32 +0530

    install api package

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    function list(){
+        return "list fun called";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+Route::get("/test",function(){
+    return ["name"=>"sho","degree"=>"BCA"];
 });
+Route::view('/home','home');   // to directly open a view
+Route::view('/welcome','welcome');
+
+Route::get("/clients",[ClientController::class,'list']);// to open call controller finction get is compulsaory
+ 
+// to pass a dynamic data
+ Route::get("/about/{name}",function($name){
+    echo $name;
+return view('about',["name"=>$name]);
+ });
